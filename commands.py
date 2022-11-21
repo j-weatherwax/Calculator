48b61d1224326f9b3bdc23c8cefcf307a39cc538
j-weatherwax <joeyw475@gmail.com>
2022-11-20 20:46:59 -0500

    Better readability and clear_entry desync fixed
    -Longer lines of code have been rewritten to aid readability
    -functionString is no longer updated for every operation and is instead created and evaluated when = is pressed.
    -precision of calculator has been limited to 8 decimal places to help mitigate errors.
    -clear_entry no longer creates a desync between the numbers in the opBox and textBox when used after a funcKey
    - When an arithmetic operation is in the string, numbers no longer appear in the textBox one by one

--- commands.py
+++ commands.py
@@ -12,8 +12,14 @@ class helperCommands():
         #String to be evaluated
         self.functionString = ""
 
+        #operation flag
         self.math = ""
+
+        #result from eval()
         self.result = ""
+
+        #sets precision of Decimal package
+        getcontext().prec = 8
 
         self.opList = ['.', '+', '-', '×', '÷', '=']
 
@@ -26,38 +32,79 @@ class helperCommands():
     def clear_entry(self):
         textVal = self.gui.textBox.get()
         
-        #prevents clearing info from self.userString and self.functionString if textBox = 0  or is empty
+        #prevents clearing info from self.userString if textBox = 0  or is empty
         if textVal == "0" or "":
             return
-        n = len(textVal)
-        self.userString = self.userString[:-n]
-        self.gui.opBox.config(text = self.userString)
-        self.functionString = self.functionString[:-n]
-        self.clear()
-        self.gui.textBox.insert("end", "0")
+            
+        #if funcKey has been used, 
+        if "(" in self.userString:
+            #remove characters up to the arithmetic operation
+            for ar_op in self.opList[1:5]:
+                if ar_op in self.userString:
+                    op_index = self.userString.index(ar_op)
+                    self.userString = self.userString[:op_index+1]
+                    self.gui.opBox.config(text = self.userString)
+                    break
+        else:        
+            n = len(textVal)
+            self.userString = self.userString[:-n]
+            self.gui.opBox.config(text = self.userString)
+        
+        self.textBoxInsert("0")
         return
+
+    #Replaces value in textBox with insertVal
+    def textBoxInsert(self, insertVal):
+        self.clear()
+        self.gui.textBox.insert("end", insertVal)
 
     def clear_all(self):
-        self.clear()
-        self.first_num = None
+        #self.first_num = None
         self.userString = ""
         self.gui.opBox.config(text = self.userString)
-        self.functionString = ""
-        self.gui.textBox.insert("end", "0")
+        self.textBoxInsert("0")
         return
 
-    #If backspace is pressed, remove last character from userString and functionString
+    def lastCharNotArithOp(self):
+        return all(ar_op != self.userString[-1] for ar_op in self.opList[1:5])
+
+    #If backspace is pressed, remove last character from userString
     def back(self):
         textVal = self.gui.textBox.get()
         #if last character is digit, delete from textbox also
         if len(textVal) > 0:
-            if all(ar_op not in self.userString for ar_op in self.opList[1:5]):
-                self.clear()
-                self.gui.textBox.insert("end", textVal[:-1])
+            if self.lastCharNotArithOp():
+                self.textBoxInsert(textVal[:-1])
             self.userString = self.userString[:-1]
             self.gui.opBox.config(text = self.userString)
-            self.functionString = self.functionString[:-1]
         return
+
+
+    def inv(self, x):
+        return 1/Decimal(x)
+
+    def square(self, x):
+        return Decimal(x)**2
+        
+    def sqroot(self, x):
+        return sqrt(Decimal(x))
+
+    def negate(self, x):
+        return -1*Decimal(x)
+
+    def funcDict(self):
+        return {'sqrt': self.sqroot, 'sqr': self.square, 'inv': self.inv, 'negate': self.negate}
+
+    def funcCalc(self, i, funcName):
+        if funcName == "perc":
+            temp = Decimal(i) / 100
+            return temp, f"{temp}"
+        else:
+            temp = f"{funcName}({i})"
+            return eval(temp, self.funcDict()), temp
+
+    def anyArithOpInUserString(self):
+        return any(ar_op in self.userString for ar_op in self.opList[1:5])
 
     def funcKey(self, func):
         textVal = self.gui.textBox.get()
@@ -67,49 +114,20 @@ class helperCommands():
         if self.userString[-1].isdigit():
             self.clear_entry()
         
-        if func == "perc":
-            temp = Decimal(textVal) / 100
-            funcTempString = f"{temp}"
-        if func == "inv":
-            temp = 1/Decimal(textVal)
-            funcTempString = f"inv({textVal})"
-        if func == "sqr":
-            temp = Decimal(textVal)**2
-            funcTempString = f"sqr({textVal})"
-        if func == "sqrt":
-            temp = sqrt(Decimal(textVal))
-            funcTempString = f"sqrt({textVal})"
-        if func == "negate":
-            temp = -1*Decimal(textVal)
-            funcTempString = f"negate({textVal})"
-
+        textBoxVal, userStrUpdate = self.funcCalc(textVal, func)
 
         #If no arithmetic operators are in the opBox, replace the previous funcKey command from the userString
-        if any(ar_op in self.userString for ar_op in self.opList[1:5]):
-            self.userString += funcTempString
+        if "=" in self.userString or not self.anyArithOpInUserString():
+            self.userString = userStrUpdate
         else:
-            self.userString = funcTempString
-            #removes previous answer from the function string to be evaluated
-            n = len(textVal)
-            self.functionString = self.functionString[:-n]
-
+            self.userString += userStrUpdate
 
         self.gui.opBox.config(text = self.userString)
-        #adds temp to functionString
-        self.functionString += str(temp)
-        self.clear()
-        self.gui.textBox.insert("end", f"{temp}")
+        self.textBoxInsert(f"{textBoxVal}")
         return
 
-
-    def funcOpFix(self, i):
-        #fix multiplication and division operators for eval function
-        funcVal = i
-        if i == "×":
-            funcVal = "*"
-        elif i == "÷":
-            funcVal = "/"
-        return funcVal
+    def noArithOpsInString(self):
+        return all(ar_op not in self.userString for ar_op in self.opList[1:5])
 
     def click(self, i):
         #if operation in oplist already exists in the user string,
@@ -122,29 +140,32 @@ class helperCommands():
             return
 
         #prevents numbers from being placed after funcKey without an arithmetic operation
-        if "(" in self.userString and str(i).isdigit() and all(ar_op not in self.userString for ar_op in self.opList[1:5]):
+        #Ex. sqrt(3)5 is not allowed but sqrt(3)+5 is
+        if "(" in self.userString and str(i).isdigit() and self.noArithOpsInString():
             return
 
         #If user presses different arithmetic operator after one already exists in the userString, replace the operator with the newly clicked operation
         #Ex. 3+ becomes 3- when - is clicked
         if len(self.userString) > 0 and self.userString[-1] in self.opList[1:5] and i in self.opList[1:5]:
             self.userString = self.userString[:-1] + i
-            funcVal = self.funcOpFix(i)
-            self.functionString = self.functionString[:-1] + funcVal
             self.gui.opBox.config(text = self.userString)
             return
 
-        funcVal = self.funcOpFix(i)
-        self.functionString += str(funcVal)
-
         #Writes to opBox
+        self.opBoxUdpate(i)
+
+        if str(i).isdigit() or i == ".":
+            self.digitHandling(i)
+
+        #resets math flag since i should be a number or . if it reaches this
+        self.math = FALSE
+
+    def opBoxUdpate(self, i):
         if "=" in self.userString and i != "=":
             #Set userString to result from equation
             self.userString = str(f"{self.result:.0f}")
             #add operation to userString
             self.userString += str(i)
-            self.functionString = str(self.result)
-            self.functionString += str(funcVal)
             self.gui.opBox.config(text = self.userString)
         else:
             #Overwrites 0 when initial number is clicked
@@ -154,13 +175,12 @@ class helperCommands():
                 self.userString += str(i)
             self.gui.opBox.config(text = self.userString)
 
-        if str(i).isdigit() or i == ".":
-            #Allows number to stay in textBox until another number is clicked
+    def digitHandling(self, i):
+        #Allows number to stay in textBox until another number is clicked
             for operation in self.opList[1:]:
-                if operation in self.userString:
+                if len(self.userString) > 1 and self.userString[-2] == operation:
                     self.clear()
             textVal = self.gui.textBox.get()
-
 
             #If textbox is just 0, don't add more 0's
             if textVal == "0":
@@ -171,31 +191,35 @@ class helperCommands():
                 #deletes . from click and add preceding 0 to decimal
                 self.userString = self.userString[:-1] + "0."
                 self.gui.opBox.config(text = self.userString)
-            self.clear()
-            self.gui.textBox.insert("end", textVal + str(i))
+            self.textBoxInsert(textVal + str(i))
 
-        #resets math flag since i should be a number or . if it reaches this
-        self.math = FALSE
-
-    def firstNumGet(self):
+    '''def firstNumGet(self):
         if "=" in self.userString:
             return
         first = self.gui.textBox.get()
-        self.first_num = Decimal(first)
+        self.first_num = Decimal(first)'''
         
     #Sets operations for addition, subtraction, multiplication, and division
     def arith(self, op):
-        self.firstNumGet()
+        #self.firstNumGet()
         self.math = TRUE
         self.click(op)
 
+    #fix multiplication and division operators for eval function
+    def evalOpFix(self, strUpdate):
+        return strUpdate.replace("×","*").replace("÷","/")
+
     def equal(self):
+        #prevents user from pressing = twice in a row or trying to evaluate an empty string
         if "=" in self.userString or self.userString == "":
             return
 
-        self.result = eval(self.functionString)
-        self.clear()
-        self.gui.textBox.insert("end", f"{self.result}")
-        self.first_num = self.result
+        #replaces mult and div symbol for evaluation
+        self.functionString = self.evalOpFix(self.userString)
+
+        #evaluates functionString and pushes result to textBox
+        self.result = eval(self.functionString, self.funcDict())
+        self.textBoxInsert(f"{self.result}")
+        #self.first_num = self.result
         self.math = FALSE
         self.click("=")
